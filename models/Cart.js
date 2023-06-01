32ea26441a831d3d27c87b8fa34d976cb425c9c9
godley0623 <godley11691@yahoo.com>
2023-06-01 09:06:59 -0400

    timeline add

---
+++ models/Cart.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const itemSchema = mongoose.Schema({
+    item: {
+        type: String,
+        required: [true, 'The name of the item is required']
+    },
+    quantity: {
+        type: Number,
+        required: [true, 'The quantity is required']
+    },
+    price: {
+        type: Number,
+        required: [true, 'A price is required']
+    }
+})
+
+const cartSchema = mongoose.Schema({
+    cart: Object
+})
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+export default Cart;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import connection from './db/connection.js';
 import dotenv from 'dotenv';
 import Card from './models/Card.js';
+import Cart from './models/Cart.js';
 
 const app = express()
 app.use(express.json())
@@ -11,7 +12,7 @@ app.use(cors());
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
-
+/*----- Card Routes -----*/
 /*----- Get Routes -----*/
 
 //Find all cards
@@ -66,6 +67,31 @@ app.get('/similar/:similar', (req, res) => {
     .catch((err) => {
         res.status(404);
         res.send({status: 404, message: 'data not found'});
+    })
+})
+
+/*----- Cart Routes -----*/
+
+/*----- GET Routes -----*/
+app.get('/cart', (req, res) => {
+    Cart.find({})
+    .then((response) => {
+        res.status(200);
+        res.send(response);
+    })
+    .catch((err) => {
+        res.status(404);
+        res.send({status: 404, message: 'data not found'})
+    })
+})
+
+/*----- POST Routes -----*/
+app.post('/cart/add', (req, res) => {
+    Cart.deleteMany({})
+    .then((response) => {
+        Cart.create(req.body);
+        res.status(200);
+        res.send({status: 200, message: "An item was added to your cart"});
     })
 })
 
